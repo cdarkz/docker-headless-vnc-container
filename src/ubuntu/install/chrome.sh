6922b86d19f963237c90140920c3d9554b3f27ee
Casper Chen <casper_chen@usiglobal.com>
2025-08-21 15:32:55 +0800

    [Ubuntu] Fix install chrome error

--- src/ubuntu/install/chrome.sh
+++ src/ubuntu/install/chrome.sh
@@ -2,7 +2,24 @@
 ### every exit != 0 fails the script
 set -e
 
+echo "Setup Debian Bookworm deb repo"
+apt-get update
+apt-get install -y ca-certificates debian-archive-keyring
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/debian-archive-keyring.gpg] http://deb.debian.org/debian bullseye main' | tee /etc/apt/sources.list.d/debian-bullseye.list
+cat <<'EOF' | tee /etc/apt/preferences.d/debian-pin
+Package: *
+Pin: release n=bullseye
+Pin-Priority: 100
+EOF
+
 echo "Install Chromium Browser"
-apt-get install -y chromium
+apt-get update
+apt-get install -y -t bullseye chromium
 ln -sfn /usr/bin/chromium /usr/bin/chromium-browser
 apt-get clean -y
+
+echo "Remove Debian Bullseye deb repo"
+rm -f /etc/apt/sources.list.d/debian-bullseye.list
+rm -f /etc/apt/preferences.d/debian-pin
+apt-get update
+# apt-get install -y --allow-downgrades libdbus-1-3=1.12.20-2ubuntu4.1
